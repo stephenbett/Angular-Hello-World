477b09cf6346a1b34c2319abc1eb69d1312698b9
stephenbett <bettstephen110@gmail.com>
2023-06-02 08:59:35 +0300

    changes-app.module file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app/app-routing.module';
+// import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
