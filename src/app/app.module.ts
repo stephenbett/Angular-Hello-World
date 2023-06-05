a8f8b7a8590f07a0f673f46e35382b5df90c6ff0
stephenbett <bettstephen110@gmail.com>
2023-06-05 16:04:29 +0300

    added-Bootstrap-Template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';import { AppRoutingModule } from './app-routing.module';
 import { CourseComponent } from './course/course.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { IntroComponent } from './components/intro/intro.component';
+import { CoursesComponent } from './components/courses/courses.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseComponent
+    CourseComponent,
+    NavbarComponent,
+    IntroComponent,
+    CoursesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
