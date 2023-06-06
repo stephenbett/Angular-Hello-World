921fd74a62ba5ca44718b7eeb24253bf5a61b6bc
stephenbett <bettstephen110@gmail.com>
2023-06-06 11:14:29 +0300

    Routing-the-Login_page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './componets/login/login.component';
+import { FooterComponent } from './components/footer/footer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'Login', component:LoginComponent},
+  {path: "Footer" , component:FooterComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { CourseComponent } from './course/course.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { IntroComponent } from './components/intro/intro.component';
 import { CoursesComponent } from './components/courses/courses.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './componets/login/login.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { FooterComponent } from './components/footer/footer.component';
     NavbarComponent,
     IntroComponent,
     CoursesComponent,
-    FooterComponent
+    FooterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
